fca3ea6c28c9d5938d4c3f0c710d24d6f8209d28
Przemek Rostek <you@example.comrochu9900@gmail.com>
2023-10-11 20:27:33 +0200

    Praca z wirtualnymi srodowiskami

--- Zadanka/car/car.py
+++ Zadanka/car/car.py
@@ -24,21 +24,16 @@ class Car:
         if self.tank_fuel > self.max_fuel:
             print("Próbujesz zatankować za dużo")
             self.tank_fuel = self.max_fuel
-        else:
-            return self.tank_fuel
         
-    
+        
     def drive(self, km):
         fuel_after_tank = self.tank_fuel
         fuel = (km / 100) * self.combustion
         self.tank_fuel = self.tank_fuel - fuel
         if self.tank_fuel < 0:
             self.tank_fuel = 0
-            return 'Przejechałeś tylko {} km z planowanych {} km biedaku. Jak się nie będziesz uczył pilnie programowania to dalej będizesz zapierdalał w Castoramie i będzie brakować Ci na paliwo'.format(int(abs(km - (fuel_after_tank * fuel))), km)
-        else:
-            return fuel
+        
              
-    
     def details(self):
         if self.tank_fuel < 0:
             return 'spalanie {} objętość baku {}'.format(self.combustion, 0)
@@ -50,7 +45,9 @@ car = Car(6, 30, 60)
 print(car.details())
 print(car.drive(700))
 print(car.details())
-print(car.refuel(1))
+print(car.refuel(1000))
+# print(car.drive(100))
+print(car.refuel(2))
 print(car.details())
 
 
--- Zadanka/car/test_car.py
+++ Zadanka/car/test_car.py
@@ -1,37 +1,41 @@
 import unittest
+from parameterized import parameterized
 from car import Car
 
 
-class TestCar(unittest.TestCase):  
-    def test_refuel(self):
+ 
+   
+@parameterized(
+            (6, 30, 60, 10, 40),
+            (7, 40, 70, 5, 45),
+            (8, 25, 30, 0, 25)
+    )
+def test_refuel(self, combustion, tank_fuel, max_fuel, refuel, expected):
         
-        car_1 = Car(6, 30, 60)
-        car_2 = Car(7, 40, 70)
-        car_3 = Car(8, 25, 30)    
+        car = Car(combustion, tank_fuel, max_fuel)
+          
+        self.assertEqual(car.refuel(refuel), expected)
+        
 
-        self.assertEqual(car_1.refuel(10), 40)
-        self.assertEqual(car_2.refuel(5), 45)
-        self.assertEqual(car_3.refuel(0), 25)
-
-    def test_drive(self):
+    # def test_drive(self):
        
-        car_1 = Car(6, 30, 60)
-        car_2 = Car(7, 40, 70)
-        car_3 = Car(8, 25, 30) 
+    #     car_1 = Car(6, 30, 60)
+    #     car_2 = Car(7, 40, 70)
+    #     car_3 = Car(8, 25, 30) 
 
-        self.assertEqual(car_1.drive(150), 9)
-        self.assertEqual(car_2.drive(300), 21)
-        self.assertEqual(car_3.drive(250), 20)
+    #     self.assertEqual(car_1.drive(150), 9)
+    #     self.assertEqual(car_2.drive(300), 21)
+    #     self.assertEqual(car_3.drive(250), 20)
 
-    def test_details(self):
+    # def test_details(self):
 
-        car_1 = Car(6, 30, 60)
-        car_2 = Car(7, 40, 70)
-        car_3 = Car(8, 25, 30) 
+    #     car_1 = Car(6, 30, 60)
+    #     car_2 = Car(7, 40, 70)
+    #     car_3 = Car(8, 25, 30) 
 
-        self.assertEqual(car_1.details(), "spalanie 6 objętość baku 30")
-        self.assertEqual(car_2.details(), "spalanie 7 objętość baku 40")
-        self.assertEqual(car_3.details(), "spalanie 8 objętość baku 25")
+    #     self.assertEqual(car_1.details(), "spalanie 6 objętość baku 30")
+    #     self.assertEqual(car_2.details(), "spalanie 7 objętość baku 40")
+    #     self.assertEqual(car_3.details(), "spalanie 8 objętość baku 25")
    
 
     
