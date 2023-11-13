a472a02f7525af23e3228f19140e0db11ef69172
Przemek Rostek <you@example.comrochu9900@gmail.com>
2023-11-13 20:07:37 +0100

    Sortowanie babelkowe

---
+++ Przydatne kody/bubble_sort.py
@@ -0,0 +1,17 @@
+def bubble(data):
+    
+    new_data = len(data)
+    
+    while new_data > 1:
+       
+        for i in range(0, new_data - 1):
+            if data[i] > data[i + 1]:
+                data[i], data[i + 1] = data[i + 1], data[i]
+                        
+        new_data -= 1
+        print(data)
+        
+        
+    return data
+        
+bubble([5, 3, 6, 8, 2, 1,])
