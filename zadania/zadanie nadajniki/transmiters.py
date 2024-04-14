5dc01dc8c919dad53deeb93717ae037ea8f8ba02
Przemek Rostek <rochu9900@gmail.com>
2024-04-14 23:01:09 +0200

    Dodane dataclassy

--- zadania/zadanie nadajniki/transmiters.py
+++ zadania/zadanie nadajniki/transmiters.py
@@ -1,26 +1,38 @@
 import math
+from dataclasses import dataclass
 
-t1 = (6, 11, 4)
-t2 = (8, 17, 3)
-t3 = (19, 19, 2)
-t4 = (19, 11, 4)
-t5 = (15, 7, 6)
-t6 = (12, 19, 4)
-start_point = (10, 19)
-end_point = (19, 14)
+@dataclass
+class Transmiter:
+    x: int
+    y: int
+    range: int
+
+@dataclass
+class Point:
+    x: int
+    y: int
+
+t1 = Transmiter(6, 11, 4)
+t2 = Transmiter(8, 17, 3)
+t3 = Transmiter(19, 19, 2)
+t4 = Transmiter(19, 11, 4)
+t5 = Transmiter(15, 7, 6)
+t6 = Transmiter(12, 19, 4)
+start_point = Point(10, 19)
+end_point = Point(19, 14)
 
 transmiters =  [t2, t3, t5, t4, t1, t6]
 
 def point(point: tuple[int, int], transmiter: list[tuple[int, int, int]]) -> bool:
     result = []
     for t in transmiter:
-        if (((t[0] - point[0]) ** 2 + (t[1] - point[1]) ** 2) <= (t[2] ** 2)):
+        if (((t.x - point.x) ** 2 + (t.y - point.y) ** 2) <= (t.range ** 2)):
             result.append(t)
     return result
 
 def transmiter_range(x: tuple[int, int, int], y: tuple[int, int, int]) -> bool:
     S = round(math.sqrt((y[0] - x[0]) ** 2 + (y[1] - x[1]) ** 2), 2)
-    return (abs(y[2] - x[2])) < S < abs(y[2] + x[2])
+    return (abs(y.r - x.r)) < S < abs(y.r + x.r)
   
 
 def range_path(data: list[tuple[int, int, int]]) -> set[tuple[int, int, int]]:
@@ -29,7 +41,7 @@ def range_path(data: list[tuple[int, int, int]]) -> set[tuple[int, int, int]]:
         for m in data:  
             path.append(n)
             path.append(m)
-    return set(path)
+    return path
 
 def solution(start: tuple[int, int], end: tuple[int, int], data: list[tuple[int, int, int]]) -> bool:  
     solution_1 = False
