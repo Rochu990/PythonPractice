b76a4a605ae02ba8284fa4cb52e70817456292c1
Przemek Rostek <rochu9900@gmail.com>
2024-08-01 20:30:48 +0200

    Projekt code with comas

--- Materialy/Automatyzacja Nudnych Zadań z Pythonem/Rozdzial 4/Projects/code_with_comas.py
+++ Materialy/Automatyzacja Nudnych Zadań z Pythonem/Rozdzial 4/Projects/code_with_comas.py
@@ -1,11 +1,15 @@
 def code_with_comas(words: list) -> str:
-    result = " "
-    for word in range(len(words) - 1):
-        result += str(words[word]) + " "
-    result += str("i " + words[len(words) - 1])
-    return result
+    if len(words) == 0:
+        return ""
+    elif len(words) == 1:
+        return words[0]
+    else:
+        result = ", ".join(words[:-1])
+        result += " i " + words[-1] 
+        return result
 
 
 words = ["jabłka", "banany", "tofu", "koty"]
+words2 = ["jabłka"]
 
 print(code_with_comas(words))
